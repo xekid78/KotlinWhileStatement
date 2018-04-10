243ea7bf79ce590130fe9734b153c8d146413936
Makoto Tanabe <xekid78@outlook.com>
2018-04-10 17:15:57 +0900

    1st commit

---
+++ while.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    println("** ケース１ **")
+    var i = 1
+    while (i <= 5) {
+        println("${i}だよ。")
+        i++
+    }
+    println()
+
+    println("** ケース２ **")
+    i = 5
+    while (i >= 1) {
+        println("${i}だよ。")
+        i--
+    }
+}
